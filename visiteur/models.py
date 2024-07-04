67b46f6094d92d427c2458763956017d71c3cb84
marcleord <marcleord@gmail.com>
2024-07-04 04:19:34 +0000

    create first model

--- visiteur/models.py
+++ visiteur/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# Model Visiteur
 class Visiteur(models.Model):
-    pass
+    # <nom_du_champ> = models.<Type>
+
+    id = models.BigAutoField()
+    nom_visiteur = models.TextField()
+    prenoms_visiteur = models.TextField()
+    type_piece = models.TextField()
+    numero_piece = models.TextField()
 
 
+    """
+    1. On crée le model
+
+    2. On crée les migrations -> python manage.py makemigrations
+        Une migration est un fichier qui transforme du code python en SQL 
+
+    3. On applique les migrations -> python manage.py migrate
+        Cela revient à dire à Django de d'exécuter le code SQL généré
+    """
