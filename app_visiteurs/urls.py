3a99f6d9a6294ac4c700d0c332abedba978d2f93
Harouna COULIBALY <ch@MacBook-Air-de-baley.local>
2024-07-04 04:02:20 +0000

    ajout de fichiers actualisés

--- app_visiteurs/urls.py
+++ app_visiteurs/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from visiteur import urls as urls_visiteur
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', urls_visiteur),
+    path('', include("visiteur.urls")),
 ]
