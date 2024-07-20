153df06ee5a0a57872f51ae5c2c7e55ff976f4b7
Harouna COULIBALY <ch@MacBook-Air-de-baley.local>
2024-07-20 21:52:17 +0000

    ajout de migrations

--- visiteur/models.py
+++ visiteur/models.py
@@ -4,11 +4,12 @@ from django.db import models
 class Visiteur(models.Model):
     # <nom_du_champ> = models.<Type>
 
-    id = models.BigAutoField()
+    #id = models.BigAutoField()
     nom_visiteur = models.TextField()
     prenoms_visiteur = models.TextField()
     type_piece = models.TextField()
     numero_piece = models.TextField()
+    numero_test = models.TextField()
 
 
     """
@@ -20,3 +21,11 @@ class Visiteur(models.Model):
     3. On applique les migrations -> python manage.py migrate
         Cela revient à dire à Django de d'exécuter le code SQL généré
     """
+
+class Visite(models.Model):
+    motif_visite = models.TextField()
+    heure_entree = models.DateTimeField()
+    heure_sortie = models.DateTimeField()
+    id_visiteur = models.ForeignKey(Visiteur, on_delete=models.CASCADE)
+
+
--- visiteur/views.py
+++ visiteur/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Visiteur
 
 # Create your views here.
 def index(request):
     return HttpResponse("BONJOUR")
+
+def creer(request):
+    if request.method == "GET":
+        return render(request, "creer_visiteur.html")
+    elif request.method == "POST":
+        nom = request.POST.get("nom")
+        prenoms = request.POST.get("prenoms")
+        type_piece = request.POST.get("type_piece")
+        numero_piece = request.POST.get("numero_piece")
+
+        Visiteur.objects.create(nom_visiteur = nom, prenoms_visiteur = prenoms, type_piece = type_piece, numero_piece = numero_piece)
+        # return HttpResponse("SUCCES")
+        # return render(request, "creer_visiteur.html")
+        return redirect("/creer")
+
